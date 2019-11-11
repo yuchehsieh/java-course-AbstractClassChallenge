ba172b20948ae304ef5f310979ad3428a7fa2379
Murphy Hsieh <murphyhsieh@leandev.io>
2019-11-11 13:11:58 +0800

    Test the traverse(), addItem() functionality

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
+        MyLinkedList list = new MyLinkedList(null);
+        list.traverse(list.getRoot());
+
+        String stringData = "Taipei Murphy Frank May Jason Sam Murphy";
+        String[] data = stringData.split(" "); // return the string array
+        for(String s: data) {
+            list.addItem(new Node(s));
+        }
+
+        list.traverse(list.getRoot());
 
     }
 }
